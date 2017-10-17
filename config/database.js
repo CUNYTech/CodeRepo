e0b662358811c45fb700aeade9330a3a1c31d88c
Alexander Melo <alexandermelox@gmail.com>
2017-10-17 15:41:19 -0400

    added commentes for local or mlab databse configurations

--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
 module.exports = {
+
+    // If you need to use a local databse comment out the mlab line. If you want to use mlab comment out the localhost mongodb.
+
+    // Mlab database
     // database: 'mongodb://admin1:admin1@ds161574.mlab.com:61574/coderef',
-    database: 'mongodb://localhost:27017/meanauth',
+
+    // local databse
+    database: 'mongodb://localhost:27017/meanauth', 
     secret: 'yoursecret'
 }
