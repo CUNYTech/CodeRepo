d1b4286b504e72644cee33bb5aaa235f35b097ef
Alexander Melo <alexandermelox@gmail.com>
2017-10-17 15:01:50 -0400

    added the required fields for the search component to collect the user data

--- angular-src/src/app/components/search/search.component.ts
+++ angular-src/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  search: string;
 
-  constructor() { }
+  constructor(private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    let search = this.search;
+    console.log(search);
+    this.search = '';
+    
+
   }
 
 }
