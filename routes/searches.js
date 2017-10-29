ebb478681eacd413238b7805b8b167bd6921d821
cheng <chengzhen.qu06@gmail.com>
2017-10-28 22:04:26 -0400

    now work with multiple search
    bug: return result order is weird, ex: if searched "DIV,TABLE,A", might return in the order of div,table,a, or a,div,table, or ..etc

--- routes/searches.js
+++ routes/searches.js
@@ -6,32 +6,35 @@ const Search = require('../models/search');
 // Search
 router.post('/search', (req, res, next) => {
 
-  const key = req.body.search;
-  
-  Search.getDefByKey(key, (err, search) => {
-	    if(err) throw err;
-	    if(!search){
-	    	return res.json({success: false, msg:'Key does not exist!'});
-	    }
-	    console.log(search[0].definition);
-	    for(att in search)
-	    {
-	    	if(att!=0)
-	    	{
-	    		search[0].attribute += "---" + search[att].attribute;
-	    		search[0].attDefinition += "---" + search[att].attDefinition;
-	    	}
-	    }
-	    console.log(search[0].definition);
-	    res.json({
-	    	search:{
-	    		key: search[0].key,
-	    		definition: search[0].definition,
-	    		attribute: search[0].attribute,
-	    		attDefinition: search[0].attDefinition
-	    	}
-	    });
-  });
+	const key = req.body.search;
+	const keys = key.split(",");
+	let count = 0;
+	var result = {};
+	var jsonName = 'search';
+	result[jsonName] = [];
+	for (var index = 0; index < keys.length; index++) 
+	{		
+		Search.getDefByKey(keys[index], (err, search) => {
+			if(err) throw err;
+			if(!search){
+				return res.json({success: false, msg:'Key does not exist!'});
+			}
+			for(att in search)
+			{
+				if(att!=0)
+				{
+					search[0].attribute += "---" + search[att].attribute;
+					search[0].attDefinition += "---" + search[att].attDefinition;
+				}
+			}
+			result[jsonName].push({key: search[0].key,definition: search[0].definition,attribute: search[0].attribute,attDefinition: search[0].attDefinition});
+			count++;
+			if(count == keys.length)
+			{
+				res.json(result);
+			}
+		});
+	}
 });
 
 module.exports = router;
