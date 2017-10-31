93450ee6cf6f1ccf44ad98158e34da4a7f9c2806
Robert O'Connor <rob@oconnor.ninja>
2017-10-31 15:27:24 -0400

    adapt for dotenv

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    // If you need to use a local databse comment out the mlab line. If you want to use mlab comment out the localhost mongodb.
+
+    // Mlab database
+     //database: 'mongodb://admin1:admin1@ds161574.mlab.com:61574/coderef',
+
+    // local database
+    database: process.env.MONGO_URI || 'mongodb://localhost:27017/meanauth',
+    secret: process.env.SECRET || 'yoursecret',
+
+
+
+
+
+
+}
