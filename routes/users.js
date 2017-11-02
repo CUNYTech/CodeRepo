f6516b48599e1078e8e84320e668efa18dbd7620
Droid Dev <bogleoneil@gmail.com>
2017-11-01 20:10:33 -0400

    added the forget password backend

--- routes/users.js
+++ routes/users.js
@@ -82,6 +82,46 @@ router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res,
 //Forgot
 router.get('/forgot',(req,res,next) => {
 	
+// res.json("password");
+	
+	const password1 = req.body.password1;
+	const password2 = req.body.password2;
+	
+	
+	if (password1 == password2){
+		
+		//res.json("sucess pasword matches");
+		
+		const email= req.body.email;
+			
+		User.getEmail(email, (err, User) => {
+		    // console.log(user)
+			if(err) {
+			
+			res.status(500).send(err);	
+				
+			} else  {
+			
+			User.password = password1;
+			
+				User.save((err, User) => {
+	            if (err) {
+	            	res.json(`err: ${err}`);
+	            
+	            }else{
+	            	
+	            res.json("sucess pasword changed");
+
+				}
+			});
+		
+		}
+	
+});
+			}  else {
+				
+				res.json("false");
+			}
 	
 	
 });
