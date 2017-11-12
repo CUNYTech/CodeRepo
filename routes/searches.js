7293e5143a1222473da653cdf63371a300b9213c
chengzhen06 <32024387+chengzhen06@users.noreply.github.com>
2017-11-12 09:56:20 -0500

    able to search for HTML and JS
    now has to be sent as
    {
      "type": "JS", //HTML or JS
       "search": "function_2" //key or funct
    }

--- routes/searches.js
+++ routes/searches.js
@@ -5,34 +5,70 @@ const Search = require('../models/search');
 
 // Search
 router.post('/search', (req, res, next) => {
-
-	const key = req.body.search;
-	const keys = key.split(",");
-	let count = 0;
-	var result = [];
-	for (var index = 0; index < keys.length; index++) 
-	{		
-		Search.getDefByKey(keys[index], (err, search) => {
-			if(err) throw err;
-			if(search == ""){
-				return res.json({success: false, msg:'Key does not exist!'});
-			}
-			for(att in search)
-			{
-				if(att!=0)
-				{
-					search[0].attribute += "---" + search[att].attribute;
-					search[0].attDefinition += "---" + search[att].attDefinition;
+	const type = req.body.type;
+	if(type == "HTML")
+	{
+		const key = req.body.search;
+		const keys = key.split(",");
+		let count = 0;
+		let result = [];
+		for (let index = 0; index < keys.length; index++) 
+		{		
+			Search.getDefByKey(keys[index], (err, search) => {
+				if(err) throw err;
+				count++;
+				if(search == ""){
+					if(count == 1)					
+						res.json({success: false, msg:'Key does not exist!'});
 				}
-			}
-			result.push({key: search[0].key,definition: search[0].definition,attribute: search[0].attribute,attDefinition: search[0].attDefinition});
-			count++;
-			if(count == keys.length)
-			{
-				res.json(result);
-			}
-		});
+				else
+				{
+					for(att in search)
+					{
+						if(att!=0)
+						{
+							search[0].attribute += "---" + search[att].attribute;
+							search[0].attDefinition += "---" + search[att].attDefinition;
+						}
+					}
+					result.push({key: search[0].key,definition: search[0].definition,attribute: search[0].attribute,attDefinition: search[0].attDefinition});
+					if(count == keys.length)
+					{
+						res.json(result);
+					}
+				}
+			});
+		}
 	}
+	else if(type == "JS")
+	{
+		const fun = req.body.search;
+		const funs = fun.split(",");
+		let count = 0;
+		let result = [];
+		for (let index = 0; index < funs.length; index++) 
+		{		
+			Search.getDefByFunctionJs(funs[index], (err, search) => {
+				if(err) throw err;
+				count++;
+				console.log(funs[index]);
+				console.log(search);
+				if(search == ""){
+					if(count == 1)
+						res.json({success: false, msg:'Function does not exist!'});
+				}
+				else
+				{
+					result.push({function: search[0].function,returnType: search[0].returnType, parameter: search[0].parameter, definition: search[0].functionDef});
+					if(count == funs.length)
+					{
+						res.json(result);
+					}
+				}
+			});
+		}
+	}
+	
 });
 
 module.exports = router;
