1d5868c22267ff70c4b11c81f17221980708f6b5
marcusnovoa <marcusn92@yahoo.com>
2017-11-15 15:54:31 -0500

    Updates to dashboard appearance. 3 buttons will link to different components.

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  titleLeft:string = "Find";
+  titleMid:string = "the code references";
+  titleRight:string = "you're looking for.";
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  findBtnEnter(){
+    this.titleMid = "the resources";
+  }
+  findBtnLeave(){
+    this.titleMid = "the code references";
+  }
+  postBtnEnter(){
+    this.titleLeft = "Post";
+    this.titleMid = "the resources";
+    this.titleRight = "you'd like.";
+  }
+  postBtnLeave(){
+    this.titleLeft = "Find";
+    this.titleMid = "the code references";
+    this.titleRight = "you're looking for.";
+  }
+  searchBtnEnter(){
+    this.titleLeft = "Search";
+    this.titleMid = "for the tags";
+    this.titleRight = "you need.";
+  }
+  searchBtnLeave(){
+    this.titleLeft = "Find";
+    this.titleMid = "the code references";
+    this.titleRight = "you're looking for.";
+  }
 }
