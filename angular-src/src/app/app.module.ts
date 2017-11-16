2c9c7eb6b5cff52171548a45af8cb867070658b7
marcusnovoa <marcusn92@yahoo.com>
2017-11-15 20:03:07 -0500

    Updated router link to Search component through search button on dashboard.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -29,6 +29,7 @@ const appRoutes: Routes = [
   {path: 'register', component: RegisterComponent, data: { animation: 'register' }},
   {path: 'login', component: LoginComponent, data: { animation: 'login' }},
   {path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard], data: { animation: 'dashboard' }},
+  {path: 'search', component: SearchComponent, data: { animation: 'search' }},
   {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard], data: { animation: 'profile' }}
 ]
 
