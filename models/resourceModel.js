e17e3bcc79ce296bf90104cee7ffb23c6fbd1bfb
Droid Dev <bogleoneil@gmail.com>
2017-11-22 16:08:09 -0500

    able to post resource

--- models/resourceModel.js
+++ models/resourceModel.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 const config = require('../config/database');
 
-
-
 const ResourceSchema = mongoose.Schema({
 	title: { type: String, required: true  },
 	  author: { type: String},
 	  tags: { type: String, required: true },
 	  code: { type: String, required: true },
 	  created_at: Date,
-	  updated_at: Date
+	  updated_at: Date,
+	  meta: {
+		    votes: Number,
+		  }
 	});
 
 
@@ -17,3 +18,32 @@ const ResourceSchema = mongoose.Schema({
 const Resource = module.exports = mongoose.model('User', ResourceSchema);
 
 
+//Find by Title
+module.exports.getResourceByTitle = function(title, callback) {
+ const query = {title: title}
+ User.findOne(title, callback);
+
+}
+
+//Find code
+module.exports.getResourceBycode = function(code, callback) {
+ const query = {code: code}
+ User.findOne(query, callback);
+}
+
+
+
+ResourceSchema.pre('save', function(next) {
+
+		  var recipe = this;
+		  // get the current date
+		  var currentDate = new Date();
+
+		  // change the updated_at field to current date
+		  Resource.updated_at = currentDate;
+
+		  // if created_at doesn't exist, add to that field
+		  if (!Resource.created_at){
+			  Resource.created_at = currentDate;
+		  }
+	  });
--- routes/resourcesSearch.js
+++ routes/resourcesSearch.js
@@ -1,3 +1,21 @@
-/**
- * 
- */
+const express = require('express');
+const router = express.Router();
+const config = require('../config/database');
+const Search = require('../models/search');
+
+
+
+//Register
+router.post('/postresource', (req, res, next) => {
+	let newUser = new Resource({
+		title: req.body.title,
+		author: req.body.author,
+		code: req.body.code,
+	    password: req.body.password
+	    
+	    
+	  });
+	
+	
+	
+});
